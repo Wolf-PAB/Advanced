c4ac744baf0b47e21b886005d56a7b389b2865e9
stokestore <stokestore@WOLFPAB>
2023-02-25 10:19:39 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace project1
+{
+    class test {
+        public static void Main() {
+            string Input_1 = Console.ReadLine();
+            string Input_2 = Console.ReadLine();
+            string Out =Input_1 + Input_2;
+            int num1=Convert.ToInt32(Input_1);
+            int num2=Convert.ToInt32(Input_2);
+            int sum=num1 + num2;
+            Console.WriteLine(Out);
+            Console.WriteLine(sum);
+                } 
+    }
+}
